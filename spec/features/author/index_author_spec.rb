ceaaebe25a301b2e6b754fa0304b7ea8374a79e2
Tim Riedel <timriedel00@gmail.com>
2022-10-29 16:10:24 +0200

    Failing test for #7

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 describe "Author overwiev page" do
-  it "should display a table with all authors from the database displaying name and website" do
+  it "should display the full name and website for every author and have a link to open the details page" do
     @alan = FactoryBot.create :author
-
     visit authors_path
     
     expect(page).to have_css 'table'
     within 'table' do
+      expect(page).to have_text 'Name'
+      expect(page).to have_text 'Homepage'
+
       expect(page).to have_text @alan.name
       expect(page).to have_link @alan.homepage, href: 'https://' + @alan.homepage
       expect(page).to have_link 'Show', href: author_path(@alan)
