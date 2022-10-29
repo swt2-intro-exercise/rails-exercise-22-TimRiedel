c10c6228678149a70c120ad0c8d32e81e8f7268e
Tim Riedel <timriedel00@gmail.com>
2022-10-29 15:02:50 +0200

    Failing test for #5

---
+++ spec/features/author/show_author_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "Author page" do
+  it "should display the details of a given author" do
+    @alan = FactoryBot.create :author
+    visit author_path(@alan)
+
+    expect(page).to have_text('Alan')
+    expect(page).to have_text('Turing')
+    expect(page).to have_link('http://wikipedia.de/Alan_Turing')
+  end
+end
