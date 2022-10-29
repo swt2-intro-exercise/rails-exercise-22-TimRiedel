c5b25b11b07ad2a7113b596d7f0cf0adbb032db9
Tim Riedel <timriedel00@gmail.com>
2022-10-29 16:20:29 +0200

    Failing test for #8

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -12,4 +12,14 @@ describe "Author model", type: :model do
     author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
     expect(author.name).to eq("Alan Turing")
   end
+
+  it "should have a last name" do
+    author = Author.new(first_name: "Alan", homepage: "http://wikipedia.org/Alan_Turing")
+    expect(author).to_not be_valid
+  end
+
+  it "must not have a homepage or first name" do
+    author = Author.new(last_name: "Turing")
+    expect(author).to be_valid
+  end
 end
