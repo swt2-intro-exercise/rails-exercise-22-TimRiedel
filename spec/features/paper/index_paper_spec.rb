e8d193d87527df702dea3252f8527d8ae43a48a2
Tim Riedel <timriedel00@gmail.com>
2022-11-05 14:56:34 +0100

    Failing test for #18

--- spec/features/paper/index_paper_spec.rb
+++ spec/features/paper/index_paper_spec.rb
@@ -7,4 +7,15 @@ describe "Index papers page" do
 
     expect(page).to have_xpath("//a[contains(@href, '/papers/1/edit')]")
   end
+
+  it "should have a link to delete each paper, which removes it from the database" do
+    @paper = FactoryBot.create :paper
+    visit papers_path
+
+    before_count = Paper.count
+    expect(page).to have_xpath("//a[@data-turbo-method='delete' and @href='/papers/1']")
+    find(:xpath, "//a[@data-turbo-method='delete' and @href='/papers/1']").click()
+    expect(Paper.count).to eq(before_count - 1)
+
+  end
 end
