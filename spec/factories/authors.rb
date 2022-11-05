e88718d2632d883db2a054dba299900d93f76b66
Tim Riedel <timriedel00@gmail.com>
2022-11-05 16:54:06 +0100

    Failing test for #22

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     first_name { "Alan" }
     last_name { "Turing" }
     homepage { "http://wikipedia.de/Alan_Turing" }
+
+    trait :second_author do
+      first_name { "Peter" }
+      last_name { "Plagiarist" }
+      homepage { "https://thispagedoesnotexist.com" }
+    end
   end
+
+  
 end
--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -4,6 +4,17 @@ describe "Edit paper page" do
   it "should have a single multiple select box for selecting authors" do
     @paper = FactoryBot.create :paper
     visit edit_paper_path(@paper)
-    expect(page).to have_xpath("//select[@multiple='multiple']")
+    expect(page).to have_xpath("//select[@multiple='multiple' and @size=1]")
+  end
+
+  it "should store the selection in the database on submit" do
+    @paper = FactoryBot.create :paper
+    @peter = FactoryBot.create :author, :second_author
+  
+    visit edit_paper_path(@paper)
+    expect(page).to have_xpath("//select[@multiple='multiple' and @size=2]")
+    find(:xpath, "//select[@multiple='multiple']").find(:option, @peter.name)
+    find(:xpath, "//input[@type='submit']").click()
+    # expect(@paper.authors.collect {|author| author.first_name}).to include(@peter.first_name)
   end
 end
