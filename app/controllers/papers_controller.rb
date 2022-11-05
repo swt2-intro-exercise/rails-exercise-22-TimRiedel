1b017b89a1a12a879dc9c2820597c12c54bc6845
Tim Riedel <timriedel00@gmail.com>
2022-11-05 17:31:56 +0100

    allow to filter papers index page by year

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -3,7 +3,11 @@ class PapersController < ApplicationController
 
   # GET /papers
   def index
-    @papers = Paper.all
+    if params[:year].present?
+      @papers = Paper.published(params[:year])
+    else
+      @papers = Paper.all
+    end
   end
 
   # GET /papers/1
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,5 +2,5 @@ class Paper < ApplicationRecord
   validates :title, :venue, :year, presence: true
   validates_numericality_of :year
   has_and_belongs_to_many :authors
-
+  scope :published, ->(year) { where("year == ?", year)}
 end
